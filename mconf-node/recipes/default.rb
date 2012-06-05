7d184eeb56f2ed61065ce3b6f7382b468c51552d
mrcruz <brcruz@gmail.com>
2012-06-05 17:42:25 -0300

    mconf-node recipe intial test

---
+++ mconf-node/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: mconf-node
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+file "/tmp/working_with_git" do
+  content "I'm working with git."
+end

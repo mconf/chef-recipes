613909939f47989bcbf38cbd2b10d9db819a0bee
mrcruz <brcruz@gmail.com>
2012-06-06 14:04:02 -0300

    deleting unnecessary config file

--- mconf-node/recipes/default.rb
+++ mconf-node/recipes/default.rb
@@ -2,10 +2,24 @@
 # Cookbook Name:: mconf-node
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, mconf.org
 #
 # All rights reserved - Do Not Redistribute
 #
-file "/tmp/working_with_git" do
-  content "I'm working with git."
+
+
+#file "/tmp/working_with_git" do
+#  content "I'm working with git."
+#end
+
+template "/tmp/mconf/mconf_config_file" do
+  owner "mconf"
+  group "mconf"
+  mode 00640
+end
+
+template "/tmp/mconf/mconf_1_var" do
+  owner "mconf"
+  group "mconf"
+  mode 00640
 end

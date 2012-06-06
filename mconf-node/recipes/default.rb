65d5a9d695358cde353a8681ca2a0fbc3f2410be
mrcruz <brcruz@gmail.com>
2012-06-06 15:22:08 -0300

    cb simplification

--- mconf-node/recipes/default.rb
+++ mconf-node/recipes/default.rb
@@ -12,17 +12,8 @@
 #  content "I'm working with git."
 #end
 
-
-template "/tmp/mconf/mconf_config_file" do
-    owner "mconf"
-    group "mconf"
-    mode 00640
-    source "mconf_config_file"
-
 package "git-core"
 package "htop"
 package "iftop"
 package "ant"
 package "curl"
-
-

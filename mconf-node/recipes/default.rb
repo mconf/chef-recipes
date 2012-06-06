5571ac1b93487e61ed777214e46bee5859a76422
mrcruz <brcruz@gmail.com>
2012-06-06 15:16:47 -0300

    cb simplification

--- mconf-node/recipes/default.rb
+++ mconf-node/recipes/default.rb
@@ -13,23 +13,16 @@
 #end
 
 
-if node[:platform] = "ubuntu" and node[:user] != "root"
-    
-    template "/tmp/mconf/mconf_config_file" do
-        owner "mconf"
-        group "mconf"
-        mode 00640
-        source "mconf_config_file"
-    
-    package "git-core"
-    package "htop"
-    package "iftop"
-    package "ant"
-    package "curl"
-end
-if node[:platform] != "ubuntu"
-    Chef::Log.info("A Mconf node MUST BE a fresh installation of Ubuntu 10.04 Server")
-end
-if node[:user] != "root"
-    Chef::Log.info("This script shouldn't be executed as root")
-end
+template "/tmp/mconf/mconf_config_file" do
+    owner "mconf"
+    group "mconf"
+    mode 00640
+    source "mconf_config_file"
+
+package "git-core"
+package "htop"
+package "iftop"
+package "ant"
+package "curl"
+
+

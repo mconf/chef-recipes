4bec49c519db25a8eac869bd8abc39ee22f1952e
mrcruz <brcruz@gmail.com>
2012-06-11 17:14:38 -0300

    simpler bash version

--- mconf-node/recipes/default.rb
+++ mconf-node/recipes/default.rb
@@ -14,34 +14,13 @@ package "iftop"
 package "ant"
 package "curl"
 
-
 bash "install mypackage" do
   cwd "#{Chef::Config[:file_cache_path]}"
   code <<-EOH
-    #!/bin/bash
-
-    function print_usage
-    {
-	    echo "Usage:"
-	    echo "    $0 <domain_name>"
-	    exit 1
-    }
-
-    if [ `lsb_release --description | grep 'Ubuntu 10.04' | wc -l` -eq 0 ]
-    then
-        echo "A Mconf node MUST BE a fresh installation of Ubuntu 10.04 Server"
-        exit 1
-    fi
-
-    if [ `whoami` == "root" ]
-    then
-        echo "This script shouldn't be executed as root"
-        exit 1
-    fi
-
-    echo "Updating the Ubuntu package repository"
+    Chef::Log.info("Updating the Ubuntu package repository")
     sudo apt-get update > /dev/null
 
+    Chef::Log.info("Making tools instalation")
     mkdir -p ~/tools
     cd ~/tools
     if [ -d "installation-scripts" ]
@@ -53,36 +32,5 @@ bash "install mypackage" do
         git clone git://github.com/mconf/installation-scripts.git
     fi
     cd installation-scripts/bbb-deploy/
-
-    chmod +x install-bigbluebutton.sh
-    ./install-bigbluebutton.sh
-    sudo bbb-conf --setip #{node[:mconf][:lbip]}
-
-    chmod +x install-notes.sh
-    ./install-notes.sh
-
-    chmod +x install-monitor.sh
-    ./install-monitor.sh lb.mconf.org bigbluebutton 10
-
-    VERSION=$(curl http://mconf.org:8888/mconf-node/current.txt)
-    wget -O bigbluebutton.zip "http://mconf.org:8888/mconf-node/$VERSION"
-    sudo ant -f deploy_target.xml deploy
-
-    chmod +x mconf-presentation.sh
-    ./mconf-presentation.sh
-
-    chmod +x enable-mobile-fs.sh
-    ./enable-mobile-fs.sh
-
-    echo "Restart the server to finish the installation"
-    echo "It will take a while to start the live notes server, please be patient"
   EOH
-end
-
-template "/etc/mconf/mconf.conf" do
-  source "mconf_config_file"
-  mode "0644"
-  variables(
-      :loadbalanceip => node[:mconf][:lbip]
-    )
 end

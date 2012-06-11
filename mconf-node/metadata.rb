85de795baa7540011d67601beb33f71b34a885af
mrcruz <brcruz@gmail.com>
2012-06-11 14:16:58 -0300

    metadata correction. enviroment support and license updated

--- mconf-node/metadata.rb
+++ mconf-node/metadata.rb
@@ -1,6 +1,7 @@
 maintainer       "mconf"
 maintainer_email "mconf@mconf.org"
-license          "All rights reserved"
-description      "Installs/Configures mconf-node"
+license          "GPL v3"
+description      "Installs/Configures a mconf-node"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.2"
+supports         "ubuntu", "= 10.04"

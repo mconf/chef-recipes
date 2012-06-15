89bd3a7a30a5d08dceb7b6523bdbf9daf56f8ccd
mrcruz <brcruz@gmail.com>
2012-06-15 17:17:21 -0300

    config file tamplate

--- mconf-node/recipes/default.rb
+++ mconf-node/recipes/default.rb
@@ -18,5 +18,23 @@ bash "create tools folder" do
   cwd "#{Chef::Config[:file_cache_path]}"
   code <<-EOH
     mkdir -p ~/tools
+    cd ~/tools
+    if [ -d "installation-scripts" ]
+    then
+    cd installation-scripts
+        git pull origin master
+        cd ..
+    else
+    git clone git://github.com/mconf/installation-scripts.git
+    fi
+    cd installation-scripts/bbb-deploy/
   EOH
+end
+
+template "/var/mconf/mconf.config" do
+  source "mconf.config"
+  mode "0644"
+  variables(
+      :lbip => node[:mconf][:lbip]
+    )
 end

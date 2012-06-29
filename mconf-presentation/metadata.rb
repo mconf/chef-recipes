43bc42b89a28b350d6aa86843c7408a2f4eba20c
mrcruz <brcruz@gmail.com>
2012-06-29 16:49:08 -0300

    mconf presentation recipe

---
+++ mconf-presentation/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "mconf"
+maintainer_email "mconf@mconf.org"
+license          "GPL v3"
+description      "Installs the presentation server for a mconf node"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+supports         "ubuntu", "= 10.04"

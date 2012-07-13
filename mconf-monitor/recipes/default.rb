697b630df5e180b2a4b43d7afd89d289b23e2d14
mrcruz <brcruz@gmail.com>
2012-07-13 15:29:29 -0300

    non functional version: should refactor install_monitor procedure

--- mconf-monitor/recipes/default.rb
+++ mconf-monitor/recipes/default.rb
@@ -6,11 +6,13 @@
 # All rights reserved - Do Not Redistribute
 #
 
-#package "zlib1g-dev"
-#package "git-core"
-#package "python-dev"
-#package "python-argparse"
-#package "subversion"
+package "zlib1g-dev"
+package "git-core"
+package "python-dev"
+package "python-argparse"
+package "subversion"
+
+include_recipe "psutil"
 
 directory "/var/mconf/tools/nagios/" do
   mode "0775"
@@ -70,4 +72,52 @@ script "set_up_nsca_sender" do
       EOH
 end
 
+#make monitor install
+script "install_monitor" do
+        interpreter "bash"
+        user "mconf"
+        environment({
+          'INSTANCE_TYPE' => node.default["mconf"]["instance_type"],
+          'NAGIOS_ADDRESS' => node.default["mconf"]["nagios_address"],
+          'INTERVAL' => node.default["mconf"]["interval"]
+        })
+        cwd "/home/mconf/"
+        code <<-EOH
+            function get_hostname
+            {
+                if [ `which bbb-conf | wc -l` -eq 0 ]
+                then
+                echo $(ifconfig | grep -v '127.0.0.1' | grep -E "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | head -1 | cut -d: -f2 | awk '{ print NAGIOS_ADDRESS}')
+                else
+                echo `bbb-conf --salt | grep 'URL' | tr -d ' ' | sed 's:URL\:http\://\([^:/]*\).*:\1:g'`
+                fi
+            }
 
+            if [ INSTANCE_TYPE == "nagios" ]
+            then
+            HOST="localhost"
+            else
+            HOST=`get_hostname`
+            fi
+
+            if [ INSTANCE_TYPE != "nagios" ]
+            then
+                echo "Sending the Nagios packet to start monitoring"
+                if [ INSTANCE_TYPE == "bigbluebutton" ]
+                then
+                CMD="~/tools/nagios-etc/cli/check_bbb_salt.sh $NAGIOS_ADDRESS $INTERVAL | tee /tmp/output_check_bbb_salt.txt 2>&1"
+                eval $CMD
+                # add a cron job to check if there's any modification on the BigBlueButton URL or salt
+                crontab -l | grep -v "check_bbb_salt.sh" > cron.jobs
+                echo "*/5 * * * * $CMD" >> cron.jobs
+                crontab cron.jobs
+                rm cron.jobs
+                else
+                    ~/tools/nagios-etc/cli/server_up.sh $NAGIOS_ADDRESS $INSTANCE_TYPE
+                fi
+            fi
+
+            chmod +x ~/tools/installation-scripts/bbb-deploy/start-monitor.sh
+            ~/tools/installation-scripts/bbb-deploy/start-monitor.sh $NAGIOS_ADDRESS $HOST $INTERVAL
+        EOH
+end

8c818278d2c81c4729a088ac76646c94acafb054
mrcruz <brcruz@gmail.com>
2012-07-17 15:20:15 -0300

    improvement: chef remote_file now used to call build and install only when necessary

--- mconf-monitor/recipes/default.rb
+++ mconf-monitor/recipes/default.rb
@@ -30,51 +30,55 @@ end
     end
 end
 
-#set up nsca sender
-script "nsca_build" do
-        interpreter "bash"
-        user "root"
-        cwd "/var/mconf/tools/nagios/"
-        not_if do File.exists?("/var/mconf/tools/nagios/nsca-#{node[:mconf][:ncsa_version]}.tar.gz") end
-        code <<-EOH
-            NSCA="nsca-#{node[:mconf][:ncsa_version]}"
-            NSCA_TAR="$NSCA.tar.gz"
-            wget -nc http://prdownloads.sourceforge.net/sourceforge/nagios/$NSCA_TAR
-            tar xzf $NSCA_TAR
-            cd $NSCA
-            ./configure
-            make
-            make install
-      EOH
-      notifies :run, 'script[set_up_nsca_mode]', :immediately
+#get nsca file from server and call build script if there is a new file
+remote_file "/var/mconf/tools/nagios/nsca-#{node[:mconf][:ncsa_version]}.tar.gz" do
+    source "http://prdownloads.sourceforge.net/sourceforge/nagios/nsca-#{node[:mconf][:ncsa_version]}.tar.gz"
+    mode "0644"
+    notifies :run, 'script[nsca_build]', :immediately
 end
 
-script "set_up_nsca_mode" do
-        action :nothing
-        interpreter "bash"
-        user "root"
-        cwd "/var/mconf/tools/nagios/nsca-#{node[:mconf][:ncsa_version]}"
-        code <<-EOH
-        if [ #{node[:mconf][:instance_type]} == "nagios" ]
-        then
-            cp src/nsca /usr/local/nagios/bin/
-            cp sample-config/nsca.cfg /usr/local/nagios/etc/
-            chmod a+r /usr/local/nagios/etc/nsca.cfg
-            # install as XINETD service
-            cp /var/mconf/tools/nagios/$NSCA/sample-config/nsca.xinetd /etc/xinetd.d/nsca
-            sed -i "s:\tonly_from.*:#\0:g" /etc/xinetd.d/nsca
-            chmod a+r /etc/xinetd.d/nsca
-            service xinetd restart
-        else
-            mkdir -p /usr/local/nagios/bin/ /usr/local/nagios/etc/
-            USER=`whoami`
-            chown $USER:$USER -R /usr/local/nagios
-        fi
+#build nsca and call installer
+script "nsca_build" do
+    action :nothing
+    interpreter "bash"
+    user "root"
+    cwd "/var/mconf/tools/nagios/"
+    code <<-EOH
+        tar xzf "nsca-#{node[:mconf][:ncsa_version]}.tar.gz"
+        cd "nsca-#{node[:mconf][:ncsa_version]}"
+        ./configure
+        make
+        make install
+    EOH
+    notifies :run, 'script[install_nsca]', :immediately
+end
 
-        cp src/send_nsca /usr/local/nagios/bin/
-        cp sample-config/send_nsca.cfg /usr/local/nagios/etc/
-        chmod a+r /usr/local/nagios/etc/send_nsca.cfg
-        EOH
+script "install_nsca" do
+    action :nothing
+    interpreter "bash"
+    user "root"
+    cwd "/var/mconf/tools/nagios/nsca-#{node[:mconf][:ncsa_version]}"
+    code <<-EOH
+    if [ #{node[:mconf][:instance_type]} == "nagios" ]
+    then
+        cp src/nsca /usr/local/nagios/bin/
+        cp sample-config/nsca.cfg /usr/local/nagios/etc/
+        chmod a+r /usr/local/nagios/etc/nsca.cfg
+        # install as XINETD service
+        cp /var/mconf/tools/nagios/$NSCA/sample-config/nsca.xinetd /etc/xinetd.d/nsca
+        sed -i "s:\tonly_from.*:#\0:g" /etc/xinetd.d/nsca
+        chmod a+r /etc/xinetd.d/nsca
+        service xinetd restart
+    else
+        mkdir -p /usr/local/nagios/bin/ /usr/local/nagios/etc/
+        USER=`whoami`
+        chown $USER:$USER -R /usr/local/nagios
+    fi
+
+    cp src/send_nsca /usr/local/nagios/bin/
+    cp sample-config/send_nsca.cfg /usr/local/nagios/etc/
+    chmod a+r /usr/local/nagios/etc/send_nsca.cfg
+    EOH
 end
 
 service "performance_reporter" do
@@ -92,30 +96,29 @@ end
 
 #make monitor install
 script "install_monitor" do
-        interpreter "bash"
-        user "mconf"
-        cwd "/home/mconf/"
-        code <<-EOH
-            INSTANCE_TYPE=#{node[:mconf][:instance_type]}
-            NAGIOS_ADDRESS=#{node[:mconf][:nagios_address]}
-            INTERVAL=#{node[:mconf][:interval]}
+    interpreter "bash"
+    user "mconf"
+    cwd "/home/mconf/"
+    code <<-EOH
+        INSTANCE_TYPE=#{node[:mconf][:instance_type]}
+        NAGIOS_ADDRESS=#{node[:mconf][:nagios_address]}
+        INTERVAL=#{node[:mconf][:interval]}
 
-            if [ $INSTANCE_TYPE != "nagios" ]
+        if [ $INSTANCE_TYPE != "nagios" ]
+        then
+            echo "Sending the Nagios packet to start monitoring"
+            if [ $INSTANCE_TYPE == "bigbluebutton" ]
             then
-                echo "Sending the Nagios packet to start monitoring"
-                if [ $INSTANCE_TYPE == "bigbluebutton" ]
-                then
-                    CMD="/var/mconf/tools/nagios/check_bbb_salt.sh $NAGIOS_ADDRESS $INTERVAL | tee /var/mconf/log/output_check_bbb_salt.txt 2>&1"
-                    eval $CMD
-                    # add a cron job to check if there's any modification on the BigBlueButton URL or salt
-                    crontab -l | grep -v "check_bbb_salt.sh" > cron.jobs
-                    echo "*/5 * * * * $CMD" >> cron.jobs
-                    crontab cron.jobs
-                    rm cron.jobs
-                else
-                    /var/mconf/tools/nagios/server_up.sh $NAGIOS_ADDRESS $INSTANCE_TYPE
-                fi
+                CMD="/var/mconf/tools/nagios/check_bbb_salt.sh $NAGIOS_ADDRESS $INTERVAL | tee /var/mconf/log/output_check_bbb_salt.txt 2>&1"
+                eval $CMD
+                # add a cron job to check if there's any modification on the BigBlueButton URL or salt
+                crontab -l | grep -v "check_bbb_salt.sh" > cron.jobs
+                echo "*/5 * * * * $CMD" >> cron.jobs
+                crontab cron.jobs
+                rm cron.jobs
+            else
+                /var/mconf/tools/nagios/server_up.sh $NAGIOS_ADDRESS $INSTANCE_TYPE
             fi
-        EOH
+        fi
+    EOH
 end
-

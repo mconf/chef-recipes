87c2fbeca0c587219988cd2e48c217893883e596
mrcruz <brcruz@gmail.com>
2012-07-17 16:32:00 -0300

    vim and htop recipes removed

--- htop/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Heavy Water Software Inc."
-maintainer_email "ops@heavywater.ca"
-license          "Apache 2.0"
-description      "Installs htop"
-version          "0.1"
-supports         "ubuntu"
--- vim/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "mconf.org"
-maintainer_email "mconf@mconf.org"
-license          "GPL"
-description      "Installs vim"
-version          "0.0.1"
-supports         "ubuntu"

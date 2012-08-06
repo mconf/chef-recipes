ccd9daee56eb67e995415c5caabf7080a2f6e186
Felipe Cecagno <fcecagno@gmail.com>
2012-08-06 16:21:00 -0300

    fixing some attributes

--- mconf-monitor/attributes/default.rb
+++ mconf-monitor/attributes/default.rb
@@ -1,9 +1,8 @@
-default[:mconf][:nagios_address] = "localhost"
 default[:mconf][:instance_type] = "bigbluebutton"
 default[:mconf][:interval] = "10"
 default[:mconf][:nagios][:dir] = "/var/mconf/tools/nagios"
 # space separated values
-default[:mconf][:monitoring_servers] = "lb.mconf.org lb1.mconf.org"
+default[:mconf][:monitoring_servers] = "lb.mconf.org"
 
 default[:nsca][:version] = "2.7.2"
 default[:nsca][:dir] = "/usr/local/nagios/bin"

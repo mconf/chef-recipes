30c930d09882222e9832b36e7ee58b3bdf05c29a
Mauricio Rodrigues Cruz <brcruz@gmail.com>
2012-08-08 17:54:43 -0300

    setting in invalid url as a monitoring server

--- mconf-monitor/attributes/default.rb
+++ mconf-monitor/attributes/default.rb
@@ -2,7 +2,7 @@ default[:mconf][:instance_type] = "bigbluebutton"
 default[:mconf][:interval] = "10"
 default[:mconf][:nagios][:dir] = "/var/mconf/tools/nagios"
 # space separated values
-default[:mconf][:monitoring_servers] = "lb.mconf.org"
+default[:mconf][:monitoring_servers] = "lb1.mconf.org"
 
 default[:nsca][:version] = "2.7.2"
 default[:nsca][:dir] = "/usr/local/nagios/bin"

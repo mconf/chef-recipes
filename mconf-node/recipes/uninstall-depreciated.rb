f73a66298c94b32386a2ca8ec7af6184b703a7d1
Felipe Cecagno <fcecagno@gmail.com>
2012-08-08 14:40:44 -0300

    cron jobs removal and other things seem to be working fine, will run it again in a clean environment

--- mconf-node/recipes/uninstall-depreciated.rb
+++ mconf-node/recipes/uninstall-depreciated.rb
@@ -1,22 +1,50 @@
-%w{ "/usr/bin/live-notes-server.sh" "~/tools/nagios-etc/cli/performance_report.py" "~/tools/nagios-etc/cli/check_bbb_salt.sh" }.each do |cmd|
-  %w{ "root" "mconf"}.each do |usr|
+users = `cat /etc/passwd | cut -d: -f1`.split()
+
+=begin
+%w{ /usr/bin/live-notes-server.sh ~/tools/nagios-etc/cli/performance_report.py ~/tools/nagios-etc/cli/check_bbb_salt.sh }.each do |cmd|
+  users.each do |usr|
     cron "remove #{cmd} cronjob as #{usr}" do
-      user usr
-      command cmd
+      user "#{usr}"
+      command "#{cmd}"
       action :delete
+    end
+  end
+=end
+
+# remove the cron jobs
+users.each do |usr|
+  %w{ /usr/bin/live-notes-server.sh ~/tools/nagios-etc/cli/performance_report.py ~/tools/nagios-etc/cli/check_bbb_salt.sh }.each do |cmd|
+    execute "remove #{cmd} cronjob as #{usr}" do
+      user "#{usr}"
+      command "crontab -l | grep -v '#{cmd}' > /tmp/cron.jobs; crontab /tmp/cron.jobs; rm /tmp/cron.jobs"
+      action :run
     end
   end
 end
 
-%{ "/usr/bin/sbt-launch.jar" "/usr/bin/sbt" "/usr/bin/live-notes-server.sh" }.each do |f|
-  file f do
+# kill the related processes
+%w{ "performance_report.py" "live-notes-server.sh" "sbt-launch.jar" }.each do |proc|
+  execute "kill process #{proc}" do
+    command "killall #{proc}; exit 0"
+    user "root"
+  end
+end
+
+# remove the applications placed into /usr/bin
+%w{ /usr/bin/sbt-launch.jar /usr/bin/sbt /usr/bin/live-notes-server.sh }.each do |f|
+  file "#{f}" do
     action :delete
   end
 end
 
-`cat /etc/passwd | cut -d: -f1`.split().each do |usr|
-  directory "/home/#{usr}/tools/"
-  action :delete
-  recursive true
+# remove the source folders
+users.each do |usr|
+  %w{ tools downloads }.each do |dir|
+    directory "/home/#{usr}/#{dir}/" do
+      user "root"
+      action :delete
+      recursive true
+    end
+  end
 end
 

6ee59fecebe8f1d4ba4911618d9b2aa8e1f4a808
Felipe Cecagno <fcecagno@gmail.com>
2013-04-11 16:29:17 -0300

    included a config file to be used with solo

---
+++ solo.rb
@@ -0,0 +1,13 @@
+#
+# This file is part of the Mconf project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+file_cache_path "/var/chef/cache"
+cookbook_path [ File.join(Dir.pwd, 'cookbooks') ]
+role_path File.join(Dir.pwd, 'roles')
+log_level :info
+

5e7bf498509da6e545178a5c4fd0dd320b3f6758
Felipe Cecagno <fcecagno@gmail.com>
2013-07-12 16:56:54 -0300

    updating the script to install chef-client

--- utils/install-chef-client.sh
+++ utils/install-chef-client.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
+CHEF_SERVER="chef.mconf.org"
+
+set -e
+
 if [ ! -f /etc/chef/validation.pem ]
 then
       echo "No validation key found (/etc/chef/validation.pem), aborting..."
       exit 1
 fi
 
+DEPENDENCIES_PKGS_LUCID="zlib1g-dev libssl-dev libreadline5-dev libyaml-dev build-essential bison checkinstall libffi5 gcc checkinstall libreadline5 libyaml-0-2"
+DEPENDENCIES_LUCID="libc6,libffi5,libgdbm3,libncurses5,libreadline5,openssl,libyaml-0-2,zlib1g"
+DEPENDENCIES_PKGS_PRECISE="zlib1g-dev libssl-dev libreadline6-dev libyaml-dev build-essential bison checkinstall libffi6 gcc checkinstall libreadline6 libyaml-0-2"
+DEPENDENCIES_PRECISE="libc6,libffi6,libgdbm3,libncurses5,libreadline6,openssl,libyaml-0-2,zlib1g"
+
+UBUNTU_VERSION=`lsb_release --codename --short`
+if [ "$UBUNTU_VERSION" == "lucid" ]; then
+      DEPENDENCIES_PKGS="$DEPENDENCIES_PKGS_LUCID"
+      DEPENDENCIES="$DEPENDENCIES_LUCID"
+elif [ "$UBUNTU_VERSION" == "precise" ]; then
+        DEPENDENCIES_PKGS="$DEPENDENCIES_PKGS_PRECISE"
+        DEPENDENCIES="$DEPENDENCIES_PRECISE"
+else
+      echo "Unsuported Ubuntu version, aborting..."
+      exit 1
+fi
+
 # http://code.google.com/p/bigbluebutton/wiki/InstallationUbuntu#2._Install_Ruby
-sudo apt-get update && sudo apt-get -y install zlib1g-dev libssl-dev libreadline5-dev libyaml-dev build-essential bison checkinstall libffi5 gcc checkinstall libreadline5 libyaml-0-2
+sudo apt-get update && sudo apt-get -y install $DEPENDENCIES_PKGS
 
 cd /tmp
 wget -N http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.gz
@@ -24,7 +45,7 @@ sudo checkinstall -D -y\
                   --pkgname='ruby1.9.2'\
                   --pkgversion='1.9.2-p290'\
                   --provides='ruby'\
-                  --requires='libc6,libffi5,libgdbm3,libncurses5,libreadline5,openssl,libyaml-0-2,zlib1g'\
+                  --requires="$DEPENDENCIES"\
                   --maintainer=brendan.ribera@gmail.com
 sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.9.2 500 \
                          --slave /usr/bin/ri ri /usr/bin/ri1.9.2 \
@@ -39,7 +60,7 @@ sudo gem install chef --version '= 10.24.0' --no-ri --no-rdoc
 
 # http://www.thelinuxdaily.com/2010/05/echo-or-cat-multiple-lines-or-paragraph-of-text-from-within-a-shell-script/
 cat > client.rb << EOF
-chef_server_url  "http://chef.mconf.org:4000"
+chef_server_url "http://$CHEF_SERVER:4000"
 EOF
 sudo mv client.rb /etc/chef/
 
@@ -50,4 +71,3 @@ cat > first_run.json << EOF
 EOF
 
 sudo chef-client -j first_run.json
-

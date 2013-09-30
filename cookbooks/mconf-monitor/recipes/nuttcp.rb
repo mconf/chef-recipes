bdd07a8ff25a64997f33991830e88efcba09c0d7
Felipe Cecagno <fcecagno@gmail.com>
2013-09-30 00:33:24 -0300

    included the nuttcp server as an upstart service to start monitoring the server bandwidth and other network parameters

---
+++ cookbooks/mconf-monitor/recipes/nuttcp.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: mconf-monitor
+# Recipe:: nuttcp
+# Author:: Felipe Cecagno (<felipe@mconf.org>)
+#
+# This file is part of the Mconf project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+cookbook_file "/usr/local/bin/nuttcp-7.2.1" do
+  source "nuttcp-7.2.1"
+  mode "0755"
+  owner node[:mconf][:user]
+end
+
+template "nuttcp upstart" do
+  path "/etc/init/nuttcp.conf"
+  source "nuttcp.conf.erb"
+  mode "0644"
+  notifies :restart, "service[nuttcp]", :delayed
+end
+
+service "nuttcp" do
+  provider Chef::Provider::Service::Upstart
+  supports :restart => true, :start => true, :stop => true
+  action [ :enable, :start ]
+  subscribes :restart, resources()
+end

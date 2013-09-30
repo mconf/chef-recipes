fb927403e2fb474d188c51528c745b753fa92bc2
Felipe Cecagno <fcecagno@gmail.com>
2013-09-30 16:05:34 -0300

    overriding the bigbluebutton attributes on the mconf-live cookbook

---
+++ cookbooks/mconf-live/attributes/override.rb
@@ -0,0 +1,22 @@
+#
+# This file is part of the Mconf project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+override[:bbb][:ffmpeg][:install_method] = "package"
+override[:bbb][:ffmpeg][:filename] = "ffmpeg_5:2.0.1-1_amd64.deb"
+override[:bbb][:ffmpeg][:repo_url] = "http://dev.mconf.org/apt-repo/files"
+
+override[:bbb][:libvpx][:install_method] = "package"
+override[:bbb][:libvpx][:filename] = "libvpx_1.2.0-1_amd64.deb"
+override[:bbb][:libvpx][:repo_url] = "http://dev.mconf.org/apt-repo/files"
+
+override[:bbb][:openoffice][:repo_url] = "http://dev.mconf.org/apt-repo/files"
+
+override[:bbb][:bigbluebutton][:repo_url] = "http://10.0.3.131/apt-repo/ubuntu"
+override[:bbb][:bigbluebutton][:key_url] = "http://10.0.3.131/apt-repo/mconf-public.gpg"
+override[:bbb][:bigbluebutton][:components] = [ "lucid", "main" ]
+override[:bbb][:bigbluebutton][:package_name] = "mconf-live"

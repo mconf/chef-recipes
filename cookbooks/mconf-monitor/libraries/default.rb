91eb9da77884aeaed4616161b56906ab5eaeb91e
Felipe Cecagno <fcecagno@gmail.com>
2013-10-13 19:25:55 -0300

    some refactoring on trace and AS lookup

---
+++ cookbooks/mconf-monitor/libraries/default.rb
@@ -0,0 +1,116 @@
+#
+# Cookbook Name:: mconf-monitor
+# Library:: default
+# Author:: Felipe Cecagno (<felipe@mconf.org>)
+#
+# This file is part of the Mconf project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+def command_execute(command, fail_on_error = false)
+  process = {}
+  process[:status] = Open4::popen4(command) do | pid, stdin, stdout, stderr|
+      process[:output] = stdout.readlines
+      process[:errors] = stderr.readlines
+  end
+  if fail_on_error and not process[:status].success?
+    raise "Execution failed: #{Array(process[:errors]).join()}"
+  end
+  process
+end
+
+def process_trace_output(peer, output)
+  entries = []
+  # remove the first line of the output
+  output.shift()
+  output.each do |line|
+    line.gsub!(" ms", "")
+
+    segments = line.split(" ")
+    segments.delete("*")
+
+    entry = {
+      :order => segments[0].to_i
+    }
+    if segments.length > 1
+      entry[:name] = segments[1]
+      entry[:address] = segments[2][1..-2]
+      begin
+        entry[:as_lookup] = perform_as_lookup(entry[:address])
+      rescue
+        entry[:as_lookup] = nil
+      end
+      rtt = segments[3..-1]
+      if rtt
+        rtt.each_with_index do |value, idx|
+          rtt[idx] = value.to_f
+        end
+        rtt.sort!
+        entry[:rtt_min] = rtt[0]
+        entry[:rtt_max] = rtt[-1]
+        entry[:rtt_avg] = rtt.inject{ |sum, el| sum + el }.to_f / rtt.size
+        entry[:rtt_probes] = rtt.size / 10.to_f
+      end
+    else
+      entry[:address] = "UNKNOWN"
+    end
+
+    entries << entry
+
+    if entry[:address] == peer
+      # arrived to the desired peer
+      break
+    end
+    # it will make repeated whois less agressive
+    sleep 1
+  end
+  entries
+end
+
+def perform_trace(ip)
+  trace_result = {
+    :output => nil,
+    :errors => nil,
+    :entries => []
+  }
+  process = command_execute("traceroute -I -q 10 -w 10 -m 60 #{ip}")
+  trace_result[:output] = process[:output]
+  trace_result[:errors] = process[:errors]
+  if process[:status].success?
+    trace_result[:entries] = process_trace_output(ip, process[:output])
+  else
+    puts "Couldn't get the trace to #{ip}: #{Array(process[:errors]).join()}"
+  end
+  trace_result
+end
+
+def perform_as_lookup(ip)
+  whois_result = {
+    :output => nil,
+    :errors => nil,
+    :entries => []
+  }
+  process = command_execute("whois -h whois.radb.net #{ip}")
+  whois_result[:output] = process[:output]
+  whois_result[:errors] = process[:errors]
+  if process[:status].success?
+    process[:output].join().split("\n\n").each do |segment|
+      entry = {}
+      segment.split("\n").each do |line|
+        ["route", "descr", "origin", "mnt-by", "changed", "source"].each do |identifier|
+          if line.start_with? identifier
+            entry[identifier] = line.slice(identifier.length + 2, line.length - (identifier.length + 2)).strip
+            break
+          end
+        end
+      end
+      whois_result[:entries] << entry
+    end
+  else
+    puts "Couldn't get the AS lookup to #{ip}: #{Array(process[:errors]).join()}"
+  end
+  return whois_result
+end
--- cookbooks/mconf-monitor/recipes/traceroute.rb
+++ cookbooks/mconf-monitor/recipes/traceroute.rb
@@ -10,7 +10,6 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-# \TODO extract this code to a library
 chef_gem "open4" do
   version "1.3.0"
   action :install
@@ -18,93 +17,30 @@ end
 
 require 'open4'
 
-def command_execute(command, fail_on_error = false)
-  process = {}
-  process[:status] = Open4::popen4(command) do | pid, stdin, stdout, stderr|
-      Chef::Log.info("Executing: #{command}")
-
-      process[:output] = stdout.readlines
-      Chef::Log.info("stdout: #{Array(process[:output]).join()} ") unless process[:output].empty?
-
-      process[:errors] = stderr.readlines
-      Chef::Log.error("stderr: #{Array(process[:errors]).join()}") unless process[:errors].empty?
-  end
-  if fail_on_error and not process[:status].success?
-    raise "Execution failed: #{Array(process[:errors]).join()}"
-  end
-  process
-end
-
 package "traceroute"
+package "whois"
 
 ruby_block "collect topology" do
   block do
-    def process_trace_output(peer, output)
-      entry = []
-      output.each do |line|
-        line.gsub!(" ms", "")
-        segments = line.split(" ")
-        ignore_input = (segments.length < 4 or segments.length > 5)
-
-        if ignore_input
-          next
-        end
-
-        order = segments[0].to_i
-
-        if segments[1, 3].join(' ') == "* * *"
-          # ex: " 5  * * *"
-          entry << {
-            :order => order,
-            :peer => "UNKNOWN"
-          }
-        elsif segments[1, 2].join(' ') == "* *"
-          #ex: " 1  * * 200.130.35.1  13.567 ms"
-          entry << {
-            :order => order,
-            :peer => segments[3],
-            :rtt_max => segments[4]
-          }
-        elsif segments[1] == "*"
-          #ex: " 1  * 200.130.35.1  10 ms  13.567 ms"
-          entry << {
-            :order => order,
-            :peer => segments[2],
-            :rtt_avg => segments[3],
-            :rtt_max => segments[4]
-          }
-        else
-          # ex: " 1  10.0.3.1  0.087 ms  0.029 ms  0.023 ms"
-          entry << {
-            :order => order,
-            :peer => segments[1],
-            :rtt_min => segments[2],
-            :rtt_avg => segments[3],
-            :rtt_max => segments[4]
-          }
-        end
-        if entry.last[:peer] == peer
-          # arrived to the desired peer
-          break
-        end
-      end
-      entry
-    end
-
-    def perform_trace(ip)
-      trace_result = []
-      begin
-        process = command_execute("traceroute -I -n #{ip}")
-        trace_result = process_trace_output(ip, process[:output])
-      rescue
-        Chef::Log.info("Couldn't get the trace to #{ip}")
-      end
-      trace_result
-    end
-
     if node[:mconf][:remount_topology]
       node.set[:mconf][:topology] = {}
+      node.set[:mconf][:as_lookup] = nil
       node.set[:mconf][:remount_topology] = false
+    end
+
+    if node[:mconf][:as_lookup].nil?
+      node.set[:mconf][:as_lookup] = perform_as_lookup(node[:bbb][:internal_ip])
+    end
+
+    source_entry = {
+      :order => 0,
+      :name => node[:bbb][:server_domain],
+      :address => node[:bbb][:internal_ip]
+    }
+    begin
+      source_entry[:as_lookup] = node[:mconf][:as_lookup]
+    rescue
+      source_entry[:as_lookup] = nil
     end
 
     list_of_peers = []
@@ -115,11 +51,8 @@ ruby_block "collect topology" do
 
         trace_result = perform_trace(peer[:bbb][:external_ip])
 
-        trace_result << {
-          :order => 0,
-          :peer => node[:ipaddress]
-        }
-        trace_result.sort_by! { |a| a[:order] }
+        trace_result[:entries] << source_entry
+        trace_result[:entries].sort_by! { |a| a[:order] }
 
         node.set[:mconf][:topology]["#{peer[:fqdn]}"] = trace_result
         # Chef::Log.info("Topology from #{node[:fqdn]} to #{peer[:fqdn]}:\n#{trace_result}")
